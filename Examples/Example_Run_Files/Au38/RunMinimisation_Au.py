fef9de00ad238b88fcce3ced4c9bc5196169e3fb
GardenGroupUO <weage963@hotmail.co.uk>
2021-02-12 15:41:03 +1300

    update version of Organisms v3.2.3

--- Examples/Example_Run_Files/Au38/RunMinimisation_Au.py
+++ Examples/Example_Run_Files/Au38/RunMinimisation_Au.py
@@ -16,7 +16,7 @@ Other outputs: Trajectory file.
 import sys
 import time
 from asap3.Internal.BuiltinPotentials import Gupta
-from ase.optimize import BFGS, FIRE
+from ase.optimize import FIRE
 
 def Minimisation_Function(cluster,collection,cluster_name):
 	#######################################################################################
@@ -24,7 +24,7 @@ def Minimisation_Function(cluster,collection,cluster_name):
 	#######################################################################################
 	# Perform the local optimisation method on the cluster.
 	# Parameter sequence: [p, q, a, xi, r0]
-	Gupta_parameters = {'Au': [10.229, 4.0360, 0.2061, 1.7900, 2.884]}
+	Gupta_parameters = {'Cu': [10.960, 2.2780, 0.0855, 1.224, 2.556]}
 	cluster.set_calculator(Gupta(Gupta_parameters, cutoff=1000, debug=False))
 	dyn = FIRE(cluster,logfile=None)
 	startTime = time.time(); converged = False
