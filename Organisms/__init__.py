959de6779b29d34c2ed770f5d95db0d63622666c
geoffreyweal <geoffrey.weal@gmail.com>
2021-03-15 00:42:31 -0700

    update version of Organisms v3.2.6

--- Organisms/__init__.py
+++ Organisms/__init__.py
@@ -1,10 +1,19 @@
-import sys
-if sys.version_info[0] == 2:
-	raise ImportError('The Organisms program requires Python3. This is Python2.')
+# The information about the Organisms program
 
 __name__    = 'The Otago Research Genetic Algorithm for Nanoclusters, Including Structural Methods and Similarity (Organisms) Program'
 __version__ = '3.2.6'
 __author__  = 'Geoffrey Weal and Dr. Anna Garden'
+
+import sys
+if sys.version_info[0] == 2:
+	toString  = '================================================'+'\n'
+	toString += 'This is the Organisms Program: A Genetic Algorithm for Nanoclusters'+'\n'
+	toString += 'Version: '+str(__version__)+'\n'
+	toString += '\n'
+	toString += 'The Organisms program requires Python3. You are attempting to execute this program in Python2.'+'\n'
+	toString += 'Make sure you are running the Organisms program in Python3 and try again'+'\n'
+	toString += 'This program will exit before beginning'
+	raise ImportError(toString)
 
 __author_email__ = 'anna.garden@otago.ac.nz'
 __license__ = 'GNU AFFERO GENERAL PUBLIC LICENSE'
